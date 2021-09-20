abf4ff3bb2d53e7a5944ee6d763ffe4718e289af
André Luiz Vicente <andre.vicente@zup.com.br>
2021-09-20 14:11:38 -0300

    Aula 2 - Exemplo 3 - Exemplo de métodos em uma conta corrente (Comentado)

--- src/br/com/zup/AulaDeMetodos/Exemplo2/ContaCorrente.java
+++ src/br/com/zup/AulaDeMetodos/Exemplo2/ContaCorrente.java
@@ -1,6 +1,8 @@
 package br.com.zup.AulaDeMetodos.Exemplo2;
 
 public class ContaCorrente {
+    // Criação dos atributos (Características) da classe
+
     String nomeDoTitular;
     String cpf;
     String agencia;
@@ -8,6 +10,7 @@ public class ContaCorrente {
     double emprestimo = 900;
     double saldo = 0;
 
+    // Método construtor
     public ContaCorrente(String nomeDoTitular, String cpf, String agencia, String numeroDeConta){
         this.nomeDoTitular = nomeDoTitular;
         this.cpf = cpf;
@@ -15,6 +18,7 @@ public class ContaCorrente {
         this.numeroDeConta = numeroDeConta;
     }
 
+    // Mostrando dados da conta corrente
     public void mostrarDados(){
         System.out.println("Nome do Titular: " +nomeDoTitular);
         System.out.println("CPF do Titular: " +cpf);
@@ -24,25 +28,40 @@ public class ContaCorrente {
         System.out.println("Saldo: R$ "+saldo);
     }
 
+    // Método responsável por subtrair o valor do saldo
     public void depositar(double valorDoDeposito){
         saldo = saldo + valorDoDeposito;
     }
 
+    // Método responsável por aumentar o valor do saldo
     public void sacar(double valorDoSaque){
         saldo = saldo - valorDoSaque;
     }
 
+    // Método responsável por liberar o emprestimo e depositar o valor em conta
     public void liberaEmprestimo(double valor){
         emprestimo = emprestimo - valor;
+        depositar(valor);
     }
 
+    // Método responsável por validar se o usuário pode ou não receber um empréstimo
     public void solicitaEmprestimo(double valor){
         if (valor > emprestimo){
             System.out.println("Este limite não está aprovado");
         } else{
             System.out.println("Este limite está aprovado");
-            depositar(valor);
             liberaEmprestimo(valor);
         }
+    }
+
+    // Método com retorno do quanto o usuário precisa pagar de imposto (Alerta de quebra do princípio de responsabilidade única*)
+    public double calculaImposto(){
+        double resultado = 0.1 * saldo;
+        return resultado;
+    }
+
+    // Debitando imposto com calculo de imposto feito em método calculaImposto()
+    public void debitaImposto(){
+        saldo = saldo - calculaImposto();
     }
 }
--- src/br/com/zup/AulaDeMetodos/Exemplo2/Main.java
+++ src/br/com/zup/AulaDeMetodos/Exemplo2/Main.java
@@ -2,14 +2,22 @@ package br.com.zup.AulaDeMetodos.Exemplo2;
 
 public class Main {
     public static void main(String[] args) {
+        // Instanciando uma conta corrente
         ContaCorrente cc1 = new ContaCorrente("André Luiz", "055555555", "687265", "8855544");
+
+        // Testando os métodos criados
         cc1.mostrarDados();
         cc1.depositar(100);
         System.out.println("-------------------");
+
         cc1.mostrarDados();
         cc1.depositar(100);
+        System.out.println("-------------------");
+
         cc1.mostrarDados();
         cc1.sacar(50);
+        System.out.println("-------------------");
+
         cc1.mostrarDados();
         cc1.solicitaEmprestimo(80);
         cc1.mostrarDados();
